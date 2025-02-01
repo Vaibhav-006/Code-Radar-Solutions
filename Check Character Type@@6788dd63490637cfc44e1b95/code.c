93608249f70cc0e939ffe9f54088c1034987e3dc
Vaibhav-006 <vaibhavsharmma123@gmail.com>
2025-02-01 23:46:52 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,7 +8,7 @@ int main() {
     char a;
     scanf("%c", &a);
     int count=0;
-    char vow[]= ['a','e','o','u','i','/0'];
+    char vow[6]= ['a','e','o','u','i','/0'];
     for(int i=0;i<=2;i++){
         if(a==vow[i]){
             count++;
