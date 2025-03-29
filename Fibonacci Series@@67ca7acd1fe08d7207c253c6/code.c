593e9a0fa2f1a21c0d2d99e4f1ac6188cb76107f
Vaibhav-006 <vaibhavsharmma123@gmail.com>
2025-03-29 23:35:11 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,9 @@
+void fibonacciSeries(int n){
+    int first=0, second=1, next;
+    for(int i=0; i<n; i++){
+        printf("%d ", first);
+        next= first+second;
+        first=second;
+        second=next;
+    }
+}
