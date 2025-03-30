e17d7ef13fcbad1c1f81a3cf9de6f22292a3c46a
Vaibhav-006 <vaibhavsharmma123@gmail.com>
2025-03-30 10:27:32 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -11,12 +11,15 @@ int main(){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             if(i!=j){
+                continue;
+            }
+            else{
             if(arr[i]-arr[j]<diff){
                 diff= arr[i]-arr[j];
                 index1= i;
                 index2= j;
             }
-        }
+            }
         }
     }
     printf("%d %d", arr[index1], arr[index2]);
